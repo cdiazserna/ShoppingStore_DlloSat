d458f8b2c2a2f9d5bc66a9a3f31636dc0661b4ca
Carlor Jr Diaz <cjunior90@gmail.com>
2023-09-30 13:04:30 -0500

    Explanation about MVC

--- ShoppingStore_DlloSat/Controllers/CountriesController.cs
+++ ShoppingStore_DlloSat/Controllers/CountriesController.cs
@@ -7,31 +7,35 @@ namespace ShoppingStore_DlloSat.Controllers
 {
     public class CountriesController : Controller
     {
+        //Creo el parámetro ReadOnly que es el que se va a manejar para el resto de la clase: _context
         private readonly DataBaseContext _context;
 
+        //Patrón de diseño llamado INYECCIÓN DE DEPENDENCIAS
         public CountriesController(DataBaseContext context)
         {
             _context = context;
         }
 
         // GET: Countries
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index() //MÉTODOS = ACTIONS DEL CONTROLADOR
         {
-            return _context.Countries != null ?
-                        View(await _context.Countries.ToListAsync()) :
-                        Problem("Entity set 'DataBaseContext.Countries'  is null.");
+            return _context.Countries != null ? View(await _context.Countries.ToListAsync()) : Problem("Entity set 'DataBaseContext.Countries'  is null.");
+            //IF TERNARIO, el famoso IF pero simplificado..... el signo ? significa ENTONCES...... el signo : significa SINO
+
+            //El Método ToListAsync me sirve para consultar UNA LISTA
         }
 
         // GET: Countries/Details/5
-        public async Task<IActionResult> Details(Guid? id)
+        public async Task<IActionResult> Details(Guid? id) //Cédula = 123
         {
             if (id == null || _context.Countries == null)
             {
                 return NotFound();
             }
 
-            var country = await _context.Countries
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+            //El Método FirstOrDefaultAsync me sirve para consultar UN OBJETO
+
             if (country == null)
             {
                 return NotFound();
@@ -40,24 +44,24 @@ namespace ShoppingStore_DlloSat.Controllers
             return View(country);
         }
 
-        // GET: Countries/Create
+        // GET: Countries/Create //GET = SELECT
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Countries/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Countries/Create //POST = CREATE
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Name,Id,CreatedDate,ModifiedDate")] Country country)
+        public async Task<IActionResult> Create(Country country)
         {
             if (ModelState.IsValid)
             {
                 country.Id = Guid.NewGuid();
-                _context.Add(country);
-                await _context.SaveChangesAsync();
+                country.CreatedDate = DateTime.Now; //Aquí automatizo el CreatedDate de un objeto
+                _context.Add(country); //Método Add() es para crear en BD
+                await _context.SaveChangesAsync(); //aquí va a la capa MODEL y GUARDA el país en la tabla Countries
                 return RedirectToAction(nameof(Index));
             }
             return View(country);
@@ -72,20 +76,20 @@ namespace ShoppingStore_DlloSat.Controllers
                 return NotFound();
             }
 
-            var country = await _context.Countries.FindAsync(id);
+            var country = await _context.Countries.FindAsync(id); //Aquí voy a la BD y me traigo ese país con ese ID    
+
             if (country == null)
             {
                 return NotFound();
             }
+
             return View(country);
         }
 
         // POST: Countries/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPut]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Name,Id,CreatedDate,ModifiedDate")] Country country)
+        public async Task<IActionResult> Edit(Guid id, Country country)
         {
             if (id != country.Id)
             {
@@ -96,8 +100,10 @@ namespace ShoppingStore_DlloSat.Controllers
             {
                 try
                 {
-                    _context.Update(country);
-                    await _context.SaveChangesAsync();
+                    country.ModifiedDate = DateTime.Now; //Aquí automáticamente le pongo fecha de modificación a la propiedad o campo ModifiedDate de la tabla Countries.
+
+                    _context.Update(country); //Método Update() es para actualizar ese objeto en BD
+                    await _context.SaveChangesAsync(); //Aquí ya hago el update en BD
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -123,8 +129,8 @@ namespace ShoppingStore_DlloSat.Controllers
                 return NotFound();
             }
 
-            var country = await _context.Countries
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == id);
+            
             if (country == null)
             {
                 return NotFound();
@@ -134,22 +140,23 @@ namespace ShoppingStore_DlloSat.Controllers
         }
 
         // POST: Countries/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> Delete(Guid id)
         {
             if (_context.Countries == null)
             {
                 return Problem("Entity set 'DataBaseContext.Countries'  is null.");
             }
+
             var country = await _context.Countries.FindAsync(id);
             if (country != null)
             {
-                _context.Countries.Remove(country);
+                _context.Countries.Remove(country); //El método Remove() es para eliminar el país
             }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            await _context.SaveChangesAsync(); //Elimino directamente el país en la BD
+            return RedirectToAction(nameof(Index)); // Redireciono al index de País
         }
 
         private bool CountryExists(Guid id)
